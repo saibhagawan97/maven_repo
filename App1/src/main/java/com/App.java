15a893a1ffc596f06a8d8de878ab75d267014b58
sai_bhagawan <saibhagawan2222@gmail.com>
2021-12-10 19:07:31 +0530

    commiting maven file

---
+++ App1/src/main/java/com/App.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "TESTING MAVEN. it is very crazy fellow" );
+    }
+}
